e30ba06797f753d425616f537b665b9c75028dba
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-07 22:21:42 -0400

    added more to server

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 var express = require('express');
 var app = express();
-
-
+var fs = require('fs');
+var path = require('path');
 
 app.get('/edit/*', function(req, res) {
-	
+		
 });
 
-app.get('/view/*', function(req, res) {
+app.get('/view/*', function(req, res) {;
 	
 });
 
@@ -19,10 +19,35 @@ app.post('/write*', function(req, res) {
 
 });
 
-app.get('*', function(req, res) {
-
+app.get('*', function(req, res, next) {
+	res.
+	returnFile(filePath, res)
 });
 
 var server = app.listen(8081, function() {
 	console.log("Listening on port %d", server.address().port);
 });
+
+function returnFile(filePath, res) {
+	fs.readFile(filePath, "binary", function(err, file) {
+		if (err) {
+			res.writeHead(500, {"Content-Type": "text/plain"});
+			res.write(err + "\n");
+			res.end();
+			return;
+		}
+
+		var headers = {}
+
+		// set content type
+		type = mime.lookup(filePath);
+		if (!res.getHeader('content-type')) {
+			var charset = mime.charsets.lookup(type);
+			headers['Content-Type'] = type + (charset ? '; charset=' + charset : '');
+		}
+
+		res.writeHead(200, headers);
+		res.write(file, "binary");
+		res.end();
+	});
+}
