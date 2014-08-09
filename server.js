5396142c5e948a7622aa03be1828fe501ca1fae6
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-09 02:09:14 -0400

    restructured error handling. finished first iteration of server (currently untested). added error files

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var mkpath = require('mkpath');
 var mime = require('mime');
 var url = require('url');
 var bodyParser = require('body-parser')
@@ -9,12 +10,11 @@ var app = express();
 
 app.get('/edit/*', function(req, res) {
 	var filePath = returnFilePath(req, '/edit/', '/edit/'.length)
-	if (fs.statSync(filePath).isDirectory()) {
-		// filePath = path.join(filePath,'/index.html');
+	if (fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()) {
 		var uri = url.parse(req.url).pathname;
 		res.redirect(path.join(__dirname, '/view/', uri.substr('/edit/').length));
 	} else {
-		returnFile(path.join(__dirname,'/static/edit.html'), res);
+		returnFile(path.join(__dirname,'/static/edit.html'), res, 200);
 	}
 });
 
@@ -22,28 +22,86 @@ app.get('/view/*', function(req, res) {
 	var filePath = returnFilePath(req, '/storage/', '/view/'.length)
 	if (!fs.existsSync(filePath)) {
 		filePath = path.join(__dirname, '/static/404_newfile.html')
+	} else {
+		if (isFileInDirectory(filePath,path.join(__dirname, '/storage'))) {
+			if (fs.statSync(filePath).isDirectory()) {
+				returnFile(path.join(__dirname, '/static/dir.html'), res, 200, filePath);
+			} else {
+				returnFile(filePath, res, 200);
+			}
+		} else {
+			handleError(res, 550, true);
+		}
 	}
+});
 
-	if (fs.statSync(filePath).isDirectory()) {
-		returnFile(path.join(__dirname, '/static/dir.html'), res);
+app.get('/listDir*', function(req, res) {
+	var parts = getURLParts(req);
+	var filePath = returnFilePath(parts.dir, '/storage/');
+	var headers = {
+		'Content-Type':'application/json'
+	};
+
+	if (!isFileInDirectory(filePath, storageDir)) {
+		handleError(res, 550, true);
+	} else {
+		fs.readdir(filePath, function(err, files) {
+			if (err) {
+				handleError(res, 500, true);
+				return;
+			}
+
+			res.writeHead(200, headers);
+			res.end(JSON.stringify(files));
+		});
 	}
-
-	returnFile(filePath, res);
 });
 
 app.use(bodyParser.json()); // to support JSON-encoded bodies
-app.use(bodyParser.urlencoded({
+app.use(bodyParser.urlencoded({ // to support URL-encoded bodies
 	extended:true
-})); // to support URL-encoded bodies
+}));
 
 app.post('/write*', function(req, res) {
 	var filePath = req.body.path;
 	var content = req.body.content;
-	console.log(req.body);
+	var headers = {
+		'Content-Type':'text/plain'
+	};
+
+	var storageDir = path.join(__dirname, '/storage/');
+	filePath = path.join(storageDir, filePath);
+
+	if (filePath == undefined || content == undefined || filePath.charAt(filePath.length - 1) == '/') {
+		handleError(res, 500, false, filePath);
+	} else if (!isFileInDirectory(filePath, storageDir, true)) {
+		handleError(res, 550, false, filePath);
+	} else {
+		mkpath(filePath.substring(0, filePath.lastIndexOf('/')), function (err) {
+			if (err) {
+				handleError(res, 500, false, filePath);
+			} else {
+				fs.write(filePath, content, function(err) {
+					if (err) {
+						handleError(res, 500, false, filePath);
+					} else {
+						res.writeHead(200, headers);
+						res.end();
+					}
+				});
+			}
+		});
+	}
 });
 
+
 app.get('*', function(req, res, next) {
-	returnFile(returnFilePath(req, '/static/', 0), res);
+	var filePath = returnFilePath(req, '/static/');
+	if (fs.existsSync(filePath) && fs.statSync(filePath).isDirectory()) {
+		filePath = path.join(filePath,'/index.html');
+	}
+
+	returnFile(filePath, res, 200);
 });
 
 var server = app.listen(8081, function() {
@@ -51,7 +109,19 @@ var server = app.listen(8081, function() {
 });
 
 function returnFilePath(req, parentDir, substr_index) {
-	var uri = url.parse(req.url).pathname;
+	var uri;
+	if (typeof(req) == 'string') {
+		uri = req;
+	}
+
+	else {
+		uri = url.parse(req.url).pathname;
+	}
+
+	if (!uri) {
+		return false;
+	}
+
 	if (substr_index) {
 		uri = uri.substr(substr_index);
 	}
@@ -59,41 +129,95 @@ function returnFilePath(req, parentDir, substr_index) {
 	return path.join(__dirname, parentDir, uri);
 }
 
-function returnFile(filePath, res, dir) {
-	var statusCode = 200;
-	if (dir) {
-		if (fs.statSync(filePath).isDirectory()) {
-			filePath = path.join(filePath,'/index.html');
-		}
-	}
-
-	if (!fs.existsSync(filePath)) {
-		filePath = '/static/404.html';
-		statusCode = 404;
+function returnFile(filePath, res, statusCode, dir) {
+	if (!statusCode) {
+		statusCode = 200;
 	}
 
 	fs.readFile(filePath, "binary", function(err, file) {
 		if (err) {
-			statusCode = 500;
-			filePath = '/static/error.html';
+			handleError(res, 500, true);
+			return false;
 		}
 
 		var headers = {}
 		// set content type
 		type = mime.lookup(filePath);
-		if (!res.getHeader('content-type')) {
+		if (!res.getHeader('Content-Type')) {
 			var charset = mime.charsets.lookup(type);
 			headers['Content-Type'] = type + (charset ? '; charset=' + charset : '');
 		}
 
-		res.writeHead(statusCode, headers);
+		if (dir) { // if we're returning a directory page
+			fs.readdir(dir, function(err, files) {
+				if (err) {
+					handleError(res, 500, true);
+					return;
+				}
 
+				file = file.toString();
+				var ind = file.indexOf('<script type=\'text/javascript\'>');
+				file = file.substring(0, ind) + "<script type='text/javascript'>var dirs = " + JSON.stringify(files) + ";</script>" + file.substr(ind);
+
+				res.writeHead(statusCode, headers);
+				res.write(new Buffer(file), "binary");
+				res.end();
+			});
+			return;
+		}
+
+		res.writeHead(statusCode, headers);
 		res.write(file, "binary");
 		res.end();
 	});
 }
 
-function getURLParts(requestURL) {
-	var url_parts = url.parse(requestURL, true);
+var errorResponses = {
+	404:'Error: 404 Not found',
+	500:'Error: Internal error',
+	550:'Error: Permission denied'
+};
+
+var errorFiles = {
+	404:path.join(__dirname, '/static/errors/404.html'),
+	500:path.join(__dirname, '/static/errors/500.html')
+	550:path.join(__dirname, '/static/errors/550.html')
+};
+
+function handleError(res, type, html, path) {
+	// 404 = not found
+	// 500 = internal error
+	// 550 = permission dnied
+	var headers = {}
+	if (html) {
+		headers['Content-Type'] = 'text/html';
+	} else {
+		headers['Content-Type'] = 'text/plain';
+		res.writeHead(type, headers);
+		if (path) {
+			res.end(errorResponses[type] + ": " + path);
+		}
+		else {
+			res.end(errorResponses[type]);
+		}
+		return;
+	}
+
+	returnFile(errorFiles[type], res, type);
+}
+
+function getURLParts(req) {
+	var url_parts = url.parse(req.url, true);
 	var query = url_parts.query;
+}
+
+function isFileInDirectory(file, dir, notTopLevel) {
+	file = fs.realpathSync(file);
+	dir = fs.realpathSync(dir);
+	var isInDir = file.indexOf(dir) == 0;
+	if (notTopLevel) {
+		return isInDir && file.substr(dir.length + 1).indexOf('/') == -1;
+	} else {
+		return isInDir;
+	}
 }
