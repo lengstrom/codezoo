a51dc445cafbdc74cb4488da56b18bd088f061e5
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-15 01:55:47 -0400

    redid directory page. added tooltips

---
+++ static/js/helpers.js
@@ -0,0 +1,23 @@
+function join() {
+	debugger;
+	var parts = [];
+	for (var i = 0, l = arguments.length; i < l; i++) {
+		parts = parts.concat(arguments[i].split("/"));
+	}
+
+	var newParts = [];
+	for (i = 0, l = parts.length; i < l; i++) {
+		var part = parts[i];
+		if (!part || part === ".") continue;
+		if (part === "..") newParts.pop();
+		else newParts.push(part);
+	}
+
+	if (parts[0] === "") newParts.unshift("");
+	return newParts.join("/") || (newParts.length ? "/" : ".");
+}
+
+function getWritePath() {
+	var currentPath = window.location.pathname;
+	return currentPath.substr(currentPath.indexOf('/edit/') + '/edit/'.length)
+}
