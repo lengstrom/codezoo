6d5c09368127b4f1d02cb5af5510ca2bb6113fe9
Logan Engstrom <meadowstream@users.noreply.github.com>
2014-08-17 02:40:59 -0400

    Split up js files into more modular pieces

---
+++ static/js/columns.js
@@ -0,0 +1,46 @@
+$('#separator').bind('mousedown', function(e) {
+	document.body.style.cursor = 'ew-resize';
+	$('#cover').css('z-index', '2');
+	$(window).bind('mousemove', separatorMove);
+});
+
+$(window).bind('mouseup', separatorMouseUp);
+$(window).resize(windowResize);
+separatorMove(false, 300)
+
+function windowResize() {
+	$('#rcol').css('width', parseInt($(window).width(), 10) - parseInt($('#lcol').width(), 10) - 7 + 'px');
+}
+
+function separatorMove(e, s) {
+	var num;
+	if (e) {
+		num = e.clientX;
+	}
+	if (s) {
+		num = s
+	}
+	if (num < 150) { //set minimum width of left column to 150px
+		$('#lcol').css('width', '140px'); //go to the lowest possible value if it is trying to be dragged smaller
+		$('.searchbarCont').css('width' , '140px');
+		$('#rcol').css('width', parseInt($(window).width(), 10) - parseInt($('#lcol').width(), 10) - 7 + 'px');
+		return;
+	}
+
+	if (parseInt($(window).width(), 10) - num < 150) { //set minimum width of right column to 150px
+		$('#lcol').css('width', parseInt($(window).width(), 10) - 144 + 'px'); //go to the lowest possible value if it is trying to be dragged smallerq
+		$('.searchbarCont').css('width', $('#lcol').width() + 'px');
+		$('#rcol').css('width', parseInt($(window).width(), 10) - parseInt($('#lcol').width(), 10) - 7 + 'px');
+		return;
+	}
+
+	$('#lcol').css('width', num - 3 + 'px');
+	$('.searchbarCont').css('width', $('#lcol').width() + 'px');
+	$('#rcol').css('width', parseInt($(window).width(), 10) - num - 4 + 'px');
+}
+
+function separatorMouseUp(e) {
+	document.body.style.cursor = '';
+	$('#cover').css('z-index', '-1');
+	$(window).unbind('mousemove', separatorMove);
+}
